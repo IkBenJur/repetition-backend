ac38f890051bc9b24262dd36034c6736b426de04
IkBenJur <jursemail@gmail.com>
2025-10-08 21:32:52 +0200

    More test

--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ Install in CLI: go install -tags 'postgres' github.com/golang-migrate/migrate/v4
 For new migration file run: migrate create -ext sql -dir migrate/migrations -seq FILENAME
 
 Run migration: go run migrate/main.go up/down
+
+## Test
+Run test: go test -v ./... 
--- routes/user/routes_test.go
+++ routes/user/routes_test.go
@@ -3,6 +3,7 @@ package user
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -37,12 +38,34 @@ func TestUserControllersHandlers(t *testing.T) {
 			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, rr.Code)
 		}
 	})
+	t.Run("Create user when valid payload", func(t *testing.T) {
+		payload := types.RegisterUserPayload {
+			Username: "NotJur",
+			Password: "a$$word",
+		}
+		marshalled, _ := json.Marshal(payload)
+		
+		req, err := http.NewRequest(http.MethodPost, "/register", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := gin.Default()
+
+		router.POST("/register", handler.handleRegister)
+		router.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusCreated {
+			t.Errorf("expected status code %d, got %d", http.StatusCreated, rr.Code)
+		}
+	})
 }
 
 type mockUserController struct {}
 
 func (controller *mockUserController) GetUserByUsername(username string) (*types.User, error) {
-	return nil, nil
+	return nil, fmt.Errorf("Failed to find user")
 }
 func (controller *mockUserController) SaveUser(user types.User) error {
 	return nil

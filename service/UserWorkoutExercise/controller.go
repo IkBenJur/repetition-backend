916d3156582344e6e2e6562b596770afcb964567
IkBenJur <jursemail@gmail.com>
2025-11-24 20:51:37 +0100

    Removed log lines

--- service/UserWorkoutExercise/controller.go
+++ service/UserWorkoutExercise/controller.go
@@ -17,26 +17,23 @@ func NewController(db *sql.DB) *Controller {
 
 func (controller *Controller) SaveUserWorkoutExercise(workoutExercise types.UserWorkoutExercise) error {
 	tx, err := controller.db.Begin()
-	fmt.Printf("Test")
+	
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback()
 
-	fmt.Printf("Test %v", workoutExercise)
 	var userWorkoutId int64
 	err = tx.QueryRow("INSERT INTO userworkoutexercise (userworkoutid, exerciseid) VALUES ($1, $2) RETURNING id", workoutExercise.UserWorkoutId, workoutExercise.ExerciseId).Scan(&userWorkoutId)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Query")
 
 	exerciseSetStmt, err := tx.Prepare("INSERT INTO userworkoutexerciseset (userworkoutexerciseid, reps, weight) VALUES ($1, $2, $3) RETURNING id")
 	if err != nil {
 		return err
 	}
 	defer exerciseSetStmt.Close()
-	fmt.Printf("Statement")
 
 	for _, set := range workoutExercise.UserWorkoutExerciseSets {
 		_, err := exerciseSetStmt.Exec(workoutExercise.ExerciseId, set.Reps, set.Weight)
@@ -44,7 +41,6 @@ func (controller *Controller) SaveUserWorkoutExercise(workoutExercise types.User
 			return err
 		}
 	}
-	fmt.Printf("Finish")
 
 	return tx.Commit()
 }

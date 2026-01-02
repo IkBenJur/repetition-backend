db0cf8a38c3c636e680c3b44a47fd95663303f48
Jur Beijaard <jurspammer@gmail.com>
2026-01-02 23:53:13 +0100

    Edit load_presciption tables

--- migrate/migrations/000018_create_load_prescription.down.sql
+++ migrate/migrations/000018_create_load_prescription.down.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS fixed_load_prescription;
 DROP TABLE IF EXISTS percentage_one_rep_max_load_prescription;
 DROP TABLE IF EXISTS rpe_load_prescription;
+DROP TABLE IF EXISTS load_prescription;
--- migrate/migrations/000018_create_load_prescription.up.sql
+++ migrate/migrations/000018_create_load_prescription.up.sql
@@ -1,17 +1,23 @@
-CREATE TABLE IF NOT EXISTS fixed_load_prescription (
+CREATE TABLE IF NOT EXISTS load_prescription (
     id SERIAL PRIMARY KEY,
+    type_id INT NOT NULL -- Static enum in code
+);
+
+-- The different types of load_prescriptions 'share' the load_prescription IDs
+CREATE TABLE IF NOT EXISTS fixed_load_prescription (
+    id INT PRIMARY KEY REFERENCES load_prescription(id) ON DELETE CASCADE,
     weight DECIMAL(6,2) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS percentage_one_rep_max_load_prescription (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY REFERENCES load_prescription(id) ON DELETE CASCADE,
     percentage DECIMAL(5,2) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS rpe_load_prescription (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY REFERENCES load_prescription(id) ON DELETE CASCADE,
     rpe DECIMAL(3,1) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );

bfc9c3cb9f9852a5cd7caa0e2140159a7106b2d1
Jur Beijaard <jurspammer@gmail.com>
2026-01-12 21:34:28 +0100

    Migrate down fix

--- migrate/migrations/000020_add_load_prescription_to_template.down.sql
+++ migrate/migrations/000020_add_load_prescription_to_template.down.sql
@@ -2,4 +2,4 @@ ALTER TABLE template_exercise_set
     DROP COLUMN load_prescription_id;
 
 ALTER TABLE template_exercise_set
-    ADD COLUMN weight_goal DECIMAL(6,3) NOT NULL;
+    ADD COLUMN weight_goal DECIMAL(6,3);

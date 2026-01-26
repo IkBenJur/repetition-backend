3844a81d3dba277d0ad8802cf2acc06da5303810
Jur Beijaard <jurspammer@gmail.com>
2026-01-26 19:43:40 +0100

    Fix bug

--- migrate/migrations/000017_add_user_id_to_template_workout.up.sql
+++ migrate/migrations/000017_add_user_id_to_template_workout.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE workout_template ADD COLUMN user_id INT NOT NULL DEFAULT 1 REFERENCES users(id);
 
-ALTER TABLE workout_template ALTER COLUMN column_name DROP DEFAULT;
+ALTER TABLE workout_template ALTER COLUMN user_id DROP DEFAULT;
